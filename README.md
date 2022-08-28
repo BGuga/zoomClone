3d01009945f1db5d62f41140680de28a3b9e7d81
Guga <103228463+BGuga@users.noreply.github.com>
2022-08-28 21:44:04 +0900

    Cross the browser chat 구현

---
+++ README.md
@@ -0,0 +1,3 @@
+# Noom
+
+Zoom Clone using nodeJs, WebRTC and Websockets.
---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+
+const app = express();
+
+const PORT = 3000;
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const serveropen = () =>
+  console.log(`server is listening in http://localhost:${PORT}`);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
+});
+
+server.listen(PORT, serveropen);
